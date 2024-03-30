62d5d320b3db7329012f5e94ab9d6b46c7d0201c
harleyandrio <105557796+harleyandrio@users.noreply.github.com>
2024-03-30 19:32:25 -0300

    Update Config.php

---
+++ Config.php
@@ -0,0 +1,13 @@
+<?php
+
+$dbHost 'aws.connect.psdb.cloud';
+
+$dbUsername = '5o2u5gq0ccjmugi9e0g3';
+
+dbPassword = 'pscale_pw_cEBfA4oD9s7f6k0s8CZbsJG4tHIe2ktBStDOBQqcmIL';
+
+$dbName '';
+
+$conexao new mysqli($dbHost, $dbUsername,)
+
+?>
